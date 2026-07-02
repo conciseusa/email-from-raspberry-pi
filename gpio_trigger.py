78cf0a85e07cd4682929b83a2746e2e039f22eb9
Walter Spurgiasz <walter@conciseusa.com>
2026-07-02 16:54:25 -0700

    Move to rpicam-still, clean up

--- gpio_trigger.py
+++ gpio_trigger.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python3
 
+# Simple script to email a picture when a GPIO pin is triggered
+
 from time import sleep
 from datetime import datetime
 import RPi.GPIO as GPIO
-import picamera
 import subprocess
 from email_handler import Class_eMail
 
+gpio_pin = 26
 var=1
-counter = 0
 To_Email_ID = ""
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def my_callback(channel):
     if var == 1:
         sleep(1.5)  # confirm the movement by waiting 1.5 sec 
-        if GPIO.input(26): # and check again the input
+        if GPIO.input(gpio_pin): # and check again the input
             now = datetime.now()
-            #timestamp = datetime.timestamp(now)
             print(now.isoformat(), " Movement!")
 
-            #captureImage()
-            #camera = picamera.PiCamera()
-            #camera.capture(now.isoformat()+'snapshot.jpg')
-            
             pic_file = now.strftime("%Y-%m-%dT%H-%M-%S")+"cam.jpg"
-            # fix raspistill error caused by full rez
+            # fix raspistill error caused by full rez, moved to rpicam-still
             # https://www.raspberrypi.org/forums/viewtopic.php?t=232533#p1478436
-            subprocess.call("raspistill -o "+pic_file, shell=True)
+            subprocess.call("rpicam-still -o "+pic_file, shell=True)
 
             email = Class_eMail()
             email.send_HTML_Attachment_Mail(To_Email_ID, 'Movement Detected!', now.isoformat() + " Movement!", pic_file)
             del email
 
             # stop detection for x sec
-            GPIO.remove_event_detect(26)
+            GPIO.remove_event_detect(gpio_pin)
             sleep(15)
-            GPIO.add_event_detect(26, GPIO.RISING, callback=my_callback, bouncetime=300)
+            GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=my_callback, bouncetime=300)
 
-GPIO.add_event_detect(26, GPIO.RISING, callback=my_callback, bouncetime=300)
+GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=my_callback, bouncetime=300)
 
 while True:
     sleep(1)
